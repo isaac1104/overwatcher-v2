5dfa8627dc14461da6285abb3615652816c5b7d5
isaac1104 <isaac-kwon@hotmail.com>
2018-06-08 22:07:10 -0700

    conditional render

--- client/src/components/HeroDetail/HeroDetail.js
+++ client/src/components/HeroDetail/HeroDetail.js
@@ -14,15 +14,21 @@ class HeroDetail extends Component {
       return (
         <Fragment>
           <h3 className='text-capitalize'><Avatar icon='user' size='large' src={`/images/heroes/${data.name}.png`}/> {data.name}</h3>
-          <h6>K/D: {value.average.eliminationsPerLife}</h6>
-          <h6>Games Played: {value.game.gamesPlayed}</h6>
-          <h6>Games Won: {value.game.gamesWon}</h6>
-          <h6>Win % : {value.game.winPercentage}</h6>
-          <h6>Gold Medals: <Badge count={value.matchAwards.medalsGold} style={{ background: 'gold' }}/></h6>
-          <h6>Silver Medals: <Badge count={value.matchAwards.medalsSilver} style={{ background: 'silver' }}/></h6>
-          <h6>Bronze Medals: <Badge count={value.matchAwards.medalsBronze} style={{ background: '#CD7F32' }}/></h6>
+          <h6>K/D: {value.average.eliminationsPerLife || 'N/A'}</h6>
+          <h6>Games Played: {value.game.gamesPlayed || 'N/A'}</h6>
+          <h6>Games Won: {value.game.gamesWon || 'N/A'}</h6>
+          <h6>Win % : {value.game.winPercentage || 'N/A'}</h6>
+          <h6>
+            Gold Medals: <Badge count={value.matchAwards.medalsGold ? value.matchAwards.medalsGold : 0} style={{ background: 'gold' }} showZero/>
+          </h6>
+          <h6>
+            Silver Medals: <Badge count={value.matchAwards.medalsSilver ? value.matchAwards.medalsSilver : 0} style={{ background: 'silver' }} showZero/>
+          </h6>
+          <h6>
+            Bronze Medals: <Badge count={value.matchAwards.medalsBronze ? value.matchAwards.medalsBronze : 0} style={{ background: '#CD7F32' }} showZero/>
+          </h6>
           <h6>Weapon Accuracy: {value.combat.weaponAccuracy || 'N/A'}</h6>
-          <h6>Damage Done: {value.combat.damageDone}</h6>
+          <h6>Damage Done: {value.combat.damageDone || 'N/A'}</h6>
         </Fragment>
       );
     }
