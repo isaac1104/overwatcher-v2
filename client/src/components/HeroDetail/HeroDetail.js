9ecdd2b07160b271ab7ffe7cf93ebf42eb53d8dc
isaac1104 <isaac-kwon@hotmail.com>
2018-06-09 22:52:54 -0700

    more stats added

--- client/src/components/HeroDetail/HeroDetail.js
+++ client/src/components/HeroDetail/HeroDetail.js
@@ -11,49 +11,60 @@ class HeroDetail extends Component {
 
   renderHeroStats() {
     const { data, data: { value } } = this.props.heroData;
+    const gridStyle = {
+      width: '25%',
+      textAlign: 'center',
+    };
     if (value) {
       return (
         <Fragment>
-          <Card title={
-            <div className='row'>
-              <div className='col-md-4'>
-                <h3 className='text-capitalize'><Avatar icon='user' size='large' src={`/images/heroes/${data.name}.png`}/> {data.name}</h3>
-              </div>
-              <div className='col-md-8'>
-                <div className='d-flex align-items-center' style={{ justifyContent: 'space-evenly' }}>
-                  <h6 className='lead'>{value.game.gamesWon || '-'} Wins</h6>
-                  <h6 className='lead'>{value.game.timePlayed}</h6>
-                  <h6>
-                    Medals
-                    <Badge count={value.matchAwards.medalsGold ? value.matchAwards.medalsGold : 0}
-                      style={{ background: 'gold', marginLeft: '10px' }}
-                      showZero
-                      overflowCount={999}
-                    />
-                    <Divider type='vertical' />
-                    <Badge
-                      count={value.matchAwards.medalsSilver ? value.matchAwards.medalsSilver : 0}
-                      style={{ background: 'silver' }}
-                      showZero
-                      overflowCount={999}
-                    />
-                    <Divider type='vertical' />
-                    <Badge count={value.matchAwards.medalsBronze ? value.matchAwards.medalsBronze : 0}
-                      style={{ background: '#CD7F32' }}
-                      showZero
-                      overflowCount={999}
-                    />
-                  </h6>
+          <Card
+            bordered={ false }
+            title={
+              <div className='row'>
+                <div className='col-md-4'>
+                  <h3 className='text-capitalize'><Avatar icon='user' size='large' src={`/images/heroes/${data.name}.png`}/> {data.name}</h3>
+                </div>
+                <div className='col-md-8'>
+                  <div className='d-flex align-items-center' style={{ justifyContent: 'space-evenly' }}>
+                    <h6 className='lead'>{value.game.gamesWon || '-'} Wins</h6>
+                    <h6 className='lead'>{value.game.timePlayed}</h6>
+                    <h6>
+                      Medals
+                      <Badge count={value.matchAwards.medalsGold ? value.matchAwards.medalsGold : 0}
+                        style={{ background: 'gold', marginLeft: '10px' }}
+                        showZero
+                        overflowCount={999}
+                      />
+                      <Divider type='vertical' />
+                      <Badge
+                        count={value.matchAwards.medalsSilver ? value.matchAwards.medalsSilver : 0}
+                        style={{ background: 'silver' }}
+                        showZero
+                        overflowCount={999}
+                      />
+                      <Divider type='vertical' />
+                      <Badge count={value.matchAwards.medalsBronze ? value.matchAwards.medalsBronze : 0}
+                        style={{ background: '#CD7F32' }}
+                        showZero
+                        overflowCount={999}
+                      />
+                    </h6>
+                  </div>
                 </div>
               </div>
-            </div>
-          }
+            }
           >
-            <h6>K/D: {value.average.eliminationsPerLife || 'N/A'}</h6>
-            <h6>Games Played: {value.game.gamesPlayed || 'N/A'}</h6>
-            <h6>Win % : {value.game.winPercentage || 'N/A'}</h6>
-            <h6>Weapon Accuracy: {value.combat.weaponAccuracy || 'N/A'}</h6>
-            <h6>Damage Done: {value.combat.damageDone || 'N/A'}</h6>
+            <Card.Grid style={gridStyle}>K/D: {value.average.eliminationsPerLife || 'N/A'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Games Played: {value.game.gamesPlayed || 'N/A'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Win % : {value.game.winPercentage || 'N/A'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Weapon Accuracy: {value.combat.weaponAccuracy || 'N/A'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Damage Done: {value.combat.damageDone || 'N/A'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Avg. Damage: {value.average.allDamageDone || 'NA'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Hero Damage: {value.combat.heroDamageDone || 'NA'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Obj. Time: {value.combat.objectiveTime || 'NA'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Time on Fire: {value.combat.timeSpentOnFire || 'NA'}</Card.Grid>
+            <Card.Grid style={gridStyle}>Multi Kills: {value.combat.multikills || 'NA'}</Card.Grid>
           </Card>
         </Fragment>
       );
