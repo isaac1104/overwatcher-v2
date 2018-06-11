9847d03bc0fc5a43f8f517e21d5272fdf0ba2788
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 11:45:53 -0700

    font change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='container-fluid text-center mt-4'>
       <h1 className='display-3 lead text-info'>Welcome to Overwatcher V2</h1>
       <h3 className='lead text-info'>Begin by searching with a valid <code>battletag</code></h3>
-      <p className='lead text-danger'>(replace # with -)</p>
+      <p className='lead text-primary'>(replace <code>#</code> with <code>-</code>)</p>
     </div>
   )
 }
