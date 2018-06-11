e0fb04cb871ffcfaf6a306df95074ed925c4e4c3
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 12:56:12 -0700

    navbar reposition & logo added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,15 +6,15 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div className='container'>
-          <Link to='/' className='navbar-brand'>OVERWATCHER V2</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <div className='ml-auto'>
-              <SearchForm />
-            </div>
+        <Link to='/' className='navbar-brand'>
+          <img src='/images/misc/owlogo.svg' className='img-fluid' alt='logo' width='50' height= '50'/> OVERWATCHER V2
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className='ml-auto'>
+            <SearchForm />
           </div>
         </div>
       </nav>
