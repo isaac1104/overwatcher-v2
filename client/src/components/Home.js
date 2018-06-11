377b46d042c861a4d6ac2136d38642664ac39c62
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 14:08:32 -0700

    home page text edit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div className='container-fluid text-center mt-4'>
       <h1 className='display-3 lead text-info'>Welcome to Overwatcher V2</h1>
-      <h3 className='lead text-info'>Begin by searching with a valid <code>battletag</code></h3>
-      <p className='lead text-primary'>(replace <code>#</code> with <code>-</code>)</p>
+      <h3 className='text-info'>Begin by searching for a valid <code>battletag*</code> including all numbers</h3>
+      <h6 className='lead text-info'>*replace <code>#</code> with <code>-</code></h6>
     </div>
   )
 }
