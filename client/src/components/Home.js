3ae30986133b03f2e00ac2e926171aa3fc0d3d62
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 11:45:00 -0700

    font change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className='container-fluid text-center mt-4'>
       <h1 className='display-3 lead text-info'>Welcome to Overwatcher V2</h1>
-      <h3 className='lead text-info'>Begin by searching with a valid battletag</h3>
+      <h3 className='lead text-info'>Begin by searching with a valid <code>battletag</code></h3>
       <p className='lead text-danger'>(replace # with -)</p>
     </div>
   )
