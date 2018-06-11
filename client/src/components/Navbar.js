93dcd0316ff66b886f2b34c98738d2f5880fc895
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 12:52:48 -0700

    searchbar moved to right

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,13 +6,17 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className='container'>
         <Link to='/' className='navbar-brand'>OVERWATCHER V2</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <SearchForm />
+          <div className='ml-auto'>
+            <SearchForm />
+          </div>
         </div>
+      </div>
       </nav>
     );
   }
