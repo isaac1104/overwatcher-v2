c3050bb5c2b0c1206780999a572566a0f7d71eda
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-11 12:53:20 -0700

    searchbar position

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,16 +7,16 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div className='container'>
-        <Link to='/' className='navbar-brand'>OVERWATCHER V2</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className='ml-auto'>
-            <SearchForm />
+          <Link to='/' className='navbar-brand'>OVERWATCHER V2</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className='ml-auto'>
+              <SearchForm />
+            </div>
           </div>
         </div>
-      </div>
       </nav>
     );
   }
