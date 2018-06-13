e36acf3ee204ba4a11f59c67214745e10dd66227
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-13 13:40:31 -0700

    home & not found page font restyled

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
 const Home = () => {
+  const style = {
+    container: {
+      textAlign: 'center'
+    },
+    font: {
+      h1: {
+        fontSize: '60px',
+        color: '#13c2c2',
+        fontWeight: 'bold'
+      },
+      h2: {
+        fontSize: '24px',
+        color: '#13c2c2'
+      },
+      h3: {
+        fontSize: '16px',
+        color:'#ff4d4f'
+      }
+    }
+  }
+
   return (
-    <div className='container-fluid text-center mt-4'>
-      <h1 className='display-3 lead text-info'>Welcome to Overwatcher V2</h1>
-      <h3 className='text-info'>Begin by searching for a valid <code>battletag*</code> including all numbers</h3>
-      <h6 className='lead text-info'>*replace <code>#</code> with <code>-</code></h6>
+    <div style={style.container}>
+      <h1 style={style.font.h1}>Welcome to Overwatcher V2</h1>
+      <h2 style={style.font.h2}>Begin by searching for a valid battletag* including all numbers</h2>
+      <h3 style={style.font.h3}>*replace # with-</h3>
     </div>
-  )
+  );
 }
 
 export default Home;
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 const NotFound = () => {
+  const style = {
+    container: {
+      textAlign: 'center'
+    },
+    font: {
+      h1: {
+        fontSize: '60px',
+        color:'#ff4d4f',
+        fontWeight: 'bold'
+      },
+      h2: {
+        fontSize: '24px',
+        color:'#13c2c2'
+      }
+    }
+  }
+
   return (
-    <div className='text-center'>
-      <h1 className='display-4 lead text-danger'>The requested player was not found!</h1>
-      <h3 className='display-4 lead'>Please check the <code>battletag</code> and try again</h3>
+    <div style={style.container}>
+      <h1 style={style.font.h1}>The requested player was not found!</h1>
+      <h2 style={style.font.h2}>Please check the battletag and try again</h2>
     </div>
   );
 }
