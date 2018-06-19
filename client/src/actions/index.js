82ae4408a184588f2163bc84b16e197fe3daea7c
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-19 11:33:31 -0700

    refactor action types import

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,23 +1,23 @@
-import { REQUEST_PLAYER_DATA, RECEIVE_PLAYER_DATA, PLAYER_DATA_ERROR, RESET_PLAYER_DATA, FETCH_HERO_DATA, RESET_HERO_DATA } from './types';
+import * as actions from './types';
 import axios from 'axios';
 
 const requestPlayerData = () => ({
-  type: REQUEST_PLAYER_DATA,
+  type: actions.REQUEST_PLAYER_DATA,
   payload: true
 });
 
 const receivePlayerData = data => ({
-  type: RECEIVE_PLAYER_DATA,
+  type: actions.RECEIVE_PLAYER_DATA,
   payload: data
 });
 
 const playerDataError = error => ({
-  type: PLAYER_DATA_ERROR,
+  type: actions.PLAYER_DATA_ERROR,
   payload: error
 });
 
 export const resetPlayerData = () => ({
-  type: RESET_PLAYER_DATA
+  type: actions.RESET_PLAYER_DATA
 });
 
 export const fetchPlayerData = battletag => async dispatch => {
@@ -36,10 +36,10 @@ export const fetchPlayerData = battletag => async dispatch => {
 }
 
 export const fetchHeroData = hero => ({
-  type: FETCH_HERO_DATA,
+  type: actions.FETCH_HERO_DATA,
   payload: hero
 });
 
 export const resetHeroData = () => ({
-  type: RESET_HERO_DATA
+  type: actions.RESET_HERO_DATA
 });
