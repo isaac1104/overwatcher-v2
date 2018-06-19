ba72b621e81d6b5239e0293b265e33f95fb11ed0
Isaac Kwon <isaac-kwon@hotmail.com>
2018-06-19 13:00:53 -0700

    sidebar menu refactor

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Layout, Menu, Icon } from 'antd';
 const { Sider } = Layout;
@@ -23,27 +23,9 @@ class Sidebar extends Component {
         <div className="logo" />
         <Menu mode="inline" theme='dark' defaultSelectedKeys={['0']}>
           <Menu.Item key="1">
-            <NavLink to="/dashboard">
-              <Icon type="dashboard" />
-                <span className="nav-text">Dashboard</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <NavLink to="/settings/profile">
-              <Icon type="user" />
-              <span className="nav-text">User Profile</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <NavLink to="/projects">
-              <Icon type="database" />
-              <span className="nav-text">Projects</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="4">
-          <NavLink to="/search">
-              <Icon type="environment-o" />
-              <span className="nav-text">Property Search</span>
+            <NavLink to="/">
+              <Icon type="home" />
+                <span className="nav-text">Home</span>
             </NavLink>
           </Menu.Item>
         </Menu>
