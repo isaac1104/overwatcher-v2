cc880192378a999a507c0ac8bdcbfe815dfc14a5
isaac1104 <isaac-kwon@hotmail.com>
2018-11-27 21:08:40 -0800

    add catch block inside player_data api call

--- routes/api_player_data.js
+++ routes/api_player_data.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 
 module.exports = app => {
   app.get('/api/playerData', async (req,res) => {
-    const request = await axios.get(`https://ow-api.com/v1/stats/pc/us/${req.query.battletag}/complete`);
-    const { data } = request;
-    res.send(data);
+    try {
+      const request = await axios.get(`https://ow-api.com/v1/stats/pc/us/${req.query.battletag}/complete`);
+      const { data } = request;
+      res.send(data);
+    } catch (e) {
+      res.send(e);
+    }
   });
 }
